a4c172c3a1517f5d67997d86a0fa1624046e33e6
Delphine <delphine.pva@gmail.com>
2021-12-14 17:11:13 +0100

    Category model done

--- src/main/java/com/delta/blog/blog/model/Category.java
+++ src/main/java/com/delta/blog/blog/model/Category.java
@@ -1,10 +1,39 @@
 package com.delta.blog.blog.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "categories")
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
